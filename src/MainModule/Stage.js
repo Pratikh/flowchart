fc87006a16cc37dbf5e1959e6e4b1f3ae61070e0
gsangayyahiremath <gsangayyahiremath@cain.com>
2020-12-11 19:14:07 +0530

    Minor changes

--- src/MainModule/Stage.js
+++ src/MainModule/Stage.js
@@ -2,7 +2,6 @@ import * as createjs from 'createjs-module';
 
 class Stage {
   constructor() {
-    window.display = this; // eslint-disable-line no-undef
     this.stage = new createjs.Stage('myCanvas');
     this.stage.enableMouseOver(10);
     createjs.Ticker.addEventListener('tick', this.stage);
